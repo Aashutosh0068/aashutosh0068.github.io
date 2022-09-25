4ab1326bd5878aac6ddc1cc0121b241c1ea3fcd8
Aashutosh Pawar <aashutoshpawar0068@outlook.com>
2022-09-25 13:07:03 +0000

    contact

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 function navDisplay() {
+  setTimeout(() => {
   if (nav.style.display == "none") {
     document.getElementById("nav").style.display = "block";
     document.getElementById("title").style.marginBottom = "0px";
@@ -6,8 +7,9 @@ function navDisplay() {
   } else {
     document.getElementById("nav").style.display = "none";
     document.getElementById("title").style.marginBottom = "10px";
-    document.getElementById("title").style.borderBottomRightRadius = "8px";
-    document.getElementById("title").style.borderBottomLeftRadius = "8px";
+    document.getElementById("title").style.borderBottomRightRadius = "2px";
+    document.getElementById("title").style.borderBottomLeftRadius = "2px";
     document.getElementById("title").style;
   }
+},150);
 }
