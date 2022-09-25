3f915abbeafc1e1a255e32549c6e2738d36f2583
Aashutosh Pawar <aashutoshpawar0068@outlook.com>
2022-09-25 11:43:24 +0000

    prettier

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
 function navDisplay() {
-    if(nav.style.display== 'none'){
-  document.getElementById("nav").style.display = "block";
-  document.getElementById("title").style.marginBottom ="0px";
-  document.getElementById("title").style.borderRadius="0px";
-    }
-    else{
-        document.getElementById("nav").style.display="none";
-        document.getElementById("title").style.marginBottom="10px";
-        document.getElementById("title").style.borderBottomRightRadius="8px";
-        document.getElementById("title").style.borderBottomLeftRadius="8px";
-        document.getElementById("title").style
-    }
+  if (nav.style.display == "none") {
+    document.getElementById("nav").style.display = "block";
+    document.getElementById("title").style.marginBottom = "0px";
+    document.getElementById("title").style.borderRadius = "0px";
+  } else {
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("title").style.marginBottom = "10px";
+    document.getElementById("title").style.borderBottomRightRadius = "8px";
+    document.getElementById("title").style.borderBottomLeftRadius = "8px";
+    document.getElementById("title").style;
+  }
 }
